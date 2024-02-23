f152763e3468da6ff171e62d7eb12abdc45afc80
André <89102322+AndreCastro097@users.noreply.github.com>
2024-02-23 16:04:20 -0300

    Add files via upload

---
+++ projeto-gta/main.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector(".btn-plataforma");
+const plataformas = document.querySelector(".btn-plataforma .plataformas");
+
+btn.addEventListener("click", ()=>{
+    
+   plataformas.classList.toggle("ativo")
+})
